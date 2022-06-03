1ca811cacfcbcd3b4bfacbb044684f5ed1a4cade
carlosdrt1 <carlosdrt229@gmail.com>
2022-06-03 19:51:31 -0300

    project created

---
+++ src/main/java/com/chyberrhino/ChyberrhinoApplication.java
@@ -0,0 +1,13 @@
+package com.chyberrhino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChyberrhinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChyberrhinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chyberrhino/ChyberrhinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chyberrhino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChyberrhinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
