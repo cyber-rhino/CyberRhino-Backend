669bcbe7415e126578a6ba795bf7d1be701b57e2
carlosdrt1 <carlosdrt229@gmail.com>
2022-06-08 10:34:08 -0300

    Creation instance operation

--- src/main/java/com/chyberrhino/ChyberrhinoApplication.java
+++ src/main/java/com/chyberrhino/ChyberrhinoApplication.java
@@ -1,13 +1,32 @@
 package com.chyberrhino;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ChyberrhinoApplication {
+import com.chyberrhino.dao.CategoryDAO;
+import com.chyberrhino.domain.Category;
 
+@SpringBootApplication
+public class ChyberrhinoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryDAO categoryDAO;
 	public static void main(String[] args) {
 		SpringApplication.run(ChyberrhinoApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Design");
+		Category cat2 = new Category(null, "Hardware");
+		Category cat3 = new Category(null, "Software");
+		
+		categoryDAO.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 
 }
